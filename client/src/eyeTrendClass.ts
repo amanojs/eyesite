69a5fca9c33d8013c53d8b7a39c1c6d2001501d2
ryo0523a <sakaron2@yahoo.co.jp>
2021-07-14 15:00:31 +0900

    'momentの導入'

--- client/src/eyeTrendClass.ts
+++ client/src/eyeTrendClass.ts
@@ -1,16 +1,25 @@
 import $ from 'jquery'; //jqueryの適用
-import { Chart } from './chart'; //chartの適用
+import { Chart } from './chart'; //chartの適用 グラフ描画
+import moment from 'moment';
 
-export class Eye {
+export class EyeTrend {
+  date: string;
   //左目の配列
   left: number[];
   //右目の配列
   right: number[];
-  constructor(left: number[], right: number[]) {
+  //グラフで使う左目の配列
+  leftGraph: number[];
+  //グラフで使う左目の配列
+  rightGraph: number[];
+  constructor(date: string, left: number[], right: number[], leftGraph: number[], rightGraph: number[]) {
+    this.date = date;
     this.left = left;
     this.right = right;
+    this.leftGraph = leftGraph;
+    this.rightGraph = rightGraph;
   }
-
+  //値の取得
   //左目の傾向
   leftTrend(): number {
     //今回の値 popで配列の後ろの値からとってくる
@@ -18,23 +27,7 @@ export class Eye {
     //前回の値
     const backLeft: number = this.left.pop() as number;
     //最新の値と前回の値を比べる処理
-    const leftResult = nowLeft - backLeft;
-    if (leftResult >= 0) {
-      //値が0以上の場合値の表示のみ
-      $('#left_result').html(`<p>${leftResult}</p>`); //デバック用の処理
-      console.log('正常です');
-    } else if (leftResult < 0 && leftResult >= -0.49) {
-      //値が0未満及び-0.49(仮)以上の場合(トレーニング)
-      $('#left_result').html(`<p>${leftResult}</p>`); //デバック用の処理
-      //トレーニングページへ飛ばすaタグをid=training_hrefとなっているタグに表示される
-      $('#training_href').html('<a href="traning.html">トレーニングページに行く</a>');
-      console.log('視力が下がっています');
-    } else {
-      //値が-0.5(仮)以下の場合(病院紹介)
-      $('#left_result').html(`<p>${leftResult}</p>`); //デバック用の処理
-      $('#hospital_href').html('<a href="hospital.html">病院の紹介</a>');
-      console.log('視力が大幅に下がっています');
-    }
+    const leftResult: number = nowLeft - backLeft;
     return leftResult;
   }
   //右目の傾向
@@ -57,66 +50,73 @@ export class Eye {
     }
     return rightResult;
   }
+  //傾向の値によって表示する物の変更
+  alert(): number {
+    if (this.leftTrend() >= 0 || this.rightTrend() >= 0) {
+      return 0;
+      console.log('正常です');
+    } else {
+      return 1;
+    }
+  }
   //グラフ化
-}
-
-//上のクラスに組み込む予定、傾向計算の処理ができるまでとりあえずここに放置
-//グラフ描画
-function graph(): void {
-  //グラフに入れるデータ
-  const data = {
-    //日付
-    labels: ['6/1', '6/2', '6/3', '6/4', '6/5', '6/6', '6/7'],
-    datasets: [
-      //左目のデータ
-      {
-        label: '左目',
-        data: [1, 0.9, 0.8, 0.9, 1, 0.75, 1],
-        borderColor: '#00AECA',
-        lineTension: 0,
-        fill: false,
-        borderWidth: 3
-      },
-
-      //右目のデータ
-      {
-        label: '右目',
-        data: [0.6, 0.5, 0.7, 0.5, 0.6, 0.75, 0.5],
-        borderColor: '#FF6B6B',
-        lineTension: 0,
-        fill: false,
-        borderWidth: 3
-      }
-    ]
-  };
-  //グラフを描画
-  const ctx = document.querySelector<HTMLCanvasElement>('#graph'); //ctxをコンストラクタの引数にする
-  if (!ctx) return; //ctxがnullだったら処理を終了する
-  new Chart(ctx, {
-    type: 'line', //グラフの種類 type: 'グラフの種別'
-    data: data, //データ data: 値
-    options: {
-      scales: {
-        //Y軸の設定
-        y: {
-          //Y軸に表示する値の最小値設定、消すと代入された値の最小値がY軸の最小値になります
-          beginAtZero: true,
-          //Y軸の名前設定
-          title: {
-            text: '視力', //Y軸の名前
-            display: true //trueで名前表示
-          }
+  graph(): void {
+    this.date = moment().format('YYYY-MM-DD');
+    console.log(this.date);
+    //グラフに入れるデータ
+    const data = {
+      //日付
+      labels: ['6/1', '6/2', '6/3', '6/4', '6/5', '6/6', '6/7'], //for文で入れる
+      datasets: [
+        //左目のデータ
+        {
+          label: '左目',
+          data: this.leftGraph,
+          borderColor: '#00AECA',
+          lineTension: 0,
+          fill: false,
+          borderWidth: 3
         },
-        //X軸の設定
-        x: {
-          //X軸の名前設定
-          title: {
-            text: '日付', //X軸の名前
-            display: true
+
+        //右目のデータ
+        {
+          label: '右目',
+          data: this.rightGraph,
+          borderColor: '#FF6B6B',
+          lineTension: 0,
+          fill: false,
+          borderWidth: 3
+        }
+      ]
+    };
+    //グラフを描画
+    const ctx = document.querySelector<HTMLCanvasElement>('#graph'); //ctxをコンストラクタの引数にする
+    if (!ctx) return; //ctxがnullだったら処理を終了する
+    new Chart(ctx, {
+      type: 'line', //グラフの種類 type: 'グラフの種別'
+      data: data, //データ data: 値
+      options: {
+        scales: {
+          //Y軸の設定
+          y: {
+            //Y軸に表示する値の最小値設定、消すと代入された値の最小値がY軸の最小値になります
+            beginAtZero: true,
+            //Y軸の名前設定
+            title: {
+              text: '視力', //Y軸の名前
+              display: true //trueで名前表示
+            }
+          },
+          //X軸の設定
+          x: {
+            //X軸の名前設定
+            title: {
+              text: '日付', //X軸の名前
+              display: true
+            }
           }
         }
       }
-    }
-  });
+    });
+  }
 }
-graph();
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,12 @@
-import { Eye } from './eyeTrendClass'; //視力傾向クラスの適用
+import { EyeTrend } from './eyeTrendClass'; //視力傾向クラスの適用
 
-//デバック用 これしないとグラフが出力されない
-const Eye1 = new Eye([1, 0.5], [1, 0.6]);
+//左から順にleft,right,leftGraph,rightGraph
+//データ取得できるようになりしだいthis.~~に変えます
+const Eye1 = new EyeTrend([1, 0.5], [1, 0.6], [1, 1, 1, 1, 1, 1, 0.5], [0.1, 0.1, 0.1, 0.1, 0.1, 1, 0.6]);
 console.log(Eye1.leftTrend());
 console.log(Eye1.rightTrend());
+//警告の出力
+Eye1.alert();
+console.log(Eye1.alert());
+//グラフの出力
+Eye1.graph();
