3fbf6847cc2a49761c45bb1b6ca46765afd9d954
katomasato <hankun0127taktak@gmail.com>
2021-07-16 17:14:58 +0900

    APIアクセスコメント追加

--- client/src/main.ts
+++ client/src/main.ts
@@ -85,6 +85,7 @@ btn?.addEventListener(
   async () => {
     const user_id = document.querySelector<HTMLInputElement>('#user_id')?.value;
     console.log(user_id);
+    // ここでAPIアクセスしてるよ sever.tsの中
     const res = await axios.get(`http://localhost:4000/selectEyeresult?id=${user_id}`);
     console.log(res);
   },
